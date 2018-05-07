3af8ca4c954e4907d346ff7e7cd2ebd8875dff53
tfleming <tfleming26@hotmail.com>
2018-05-07 00:28:09 -0600

    Add product domain model, interface respository, and fake repository

---
+++ Models/FakeProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sportify.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product { Name = "Football", Price = 25 },
+            new Product { Name = "Baseball Glove", Price = 19 },
+            new Product { Name = "Track Shoes", Price = 97 }
+        }.AsQueryable<Product>();
+    }
+}
