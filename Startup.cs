ab97bdafb50a4e5841f0df3cb041c88b0ebc62e5
tfleming <tfleming26@hotmail.com>
2018-05-07 00:34:37 -0600

    Register repository service

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Sportify.Models;
 
 namespace Sportify
 {
@@ -15,6 +16,7 @@ namespace Sportify
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IProductRepository, FakeProductRepository>();
             services.AddMvc();
         }
 
