f4bd7c1b96587a7d3ee0bbefa9dd3000de5ac385
tfleming <tfleming26@hotmail.com>
2018-05-07 01:06:16 -0600

    Add product controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sportify.Models;
+
+namespace Sportify.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+    }
+}
