f815dcc39ae4d526f824975c9589c62954b6fdcd
tfleming <tfleming26@hotmail.com>
2018-05-10 09:18:50 -0600

    Update paginginfo class

--- Models/ViewModels/PagingInfo.cs
+++ Models/ViewModels/PagingInfo.cs
@@ -9,6 +9,7 @@ namespace Sportify.Models.ViewModels
     {
         public int TotalItems { get; set; }
         public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
         public int TotalPage { get; set; }
         public int TotalPages =>
             (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
