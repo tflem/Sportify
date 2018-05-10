40a735d9929c8e0d0d1656d734a14765d5a44643
tfleming <tfleming26@hotmail.com>
2018-05-10 12:31:06 -0600

    Add products list viewmodel class

---
+++ Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Sportify.Models;
+
+namespace Sportify.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
