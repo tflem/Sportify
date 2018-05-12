7c8fe84175aa60f1763944bccf4e059c5add0c3f
tfleming <tfleming26@hotmail.com>
2018-05-12 10:43:01 -0600

    Add category support to list action

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -15,10 +15,11 @@ namespace Sportify.Controllers
             repository = repo;
         }
 
-        public ViewResult List(int productPage = 1)
+        public ViewResult List(string category, int productPage = 1)
             => View(new ProductsListViewModel
             {
                 Products = repository.Products
+                  .Where(p => category == null || p.Category == category)
                   .OrderBy(p => p.ProductID)
                   .Skip((productPage - 1) * PageSize)
                   .Take(PageSize),
@@ -28,7 +29,8 @@ namespace Sportify.Controllers
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory = category
             });           
     }
 }
