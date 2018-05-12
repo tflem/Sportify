ad1a2f65ea7612ff9e76852a9d4735a6268a3f94
tfleming <tfleming26@hotmail.com>
2018-05-12 10:32:12 -0600

    Add current category property

--- Models/ViewModels/ProductsListViewModel.cs
+++ Models/ViewModels/ProductsListViewModel.cs
@@ -7,5 +7,6 @@ namespace Sportify.Models.ViewModels
     {
         public IEnumerable<Product> Products { get; set; }
         public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
