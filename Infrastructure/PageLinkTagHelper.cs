48bd854a3a74f13acf5b6a7c307dabe0be3ec6f1
tfleming <tfleming26@hotmail.com>
2018-05-12 19:49:39 -0600

    Add prefixed values in page link tag helper

--- Infrastructure/PageLinkTagHelper.cs
+++ Infrastructure/PageLinkTagHelper.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Sportify.Models.ViewModels;
+using System.Collections.Generic;
 
 namespace Sportify.Infrastructure
 {
@@ -20,6 +21,11 @@ namespace Sportify.Infrastructure
         public ViewContext ViewContext { get; set; }
         public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
+
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> PageUrlValues { get; set; }
+            = new Dictionary<string, object>();
+
         public bool PageClassesEnabled { get; set; } = false;
         public string PageClass { get; set; }
         public string PageClassNormal { get; set; }
@@ -33,8 +39,8 @@ namespace Sportify.Infrastructure
             for(int i = 1; i <= PageModel.TotalPages; i++)
             {
                 TagBuilder tag = new TagBuilder("a");
-                tag.Attributes["href"] = urlHelper.Action(PageAction,
-                    new { productPage = i });
+                PageUrlValues["productPage"] = i;
+                tag.Attributes["href"] = urlHelper.Action(PageAction, PageUrlValues);
                 if (PageClassesEnabled)
                 {
                     tag.AddCssClass(PageClass);
