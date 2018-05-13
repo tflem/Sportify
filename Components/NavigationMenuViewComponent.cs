8a9fdf92ab5685afea288adfb015b37fc0cb9c98
tfleming <tfleming26@hotmail.com>
2018-05-12 20:07:11 -0600

    Add navigation menu view component class

---
+++ Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sportify.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hi, what did I miss from the Nav View Component?";
+        }
+    }
+}
